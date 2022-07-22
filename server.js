6af6ca3bfbab8df4be82f008c99974c8c15d2dd9
Stoyann <stoyann.openclassrooms@gmail.com>
2022-07-23 04:28:16 +1100

    connect to database whith mongoose

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const morgan = require ('morgan');
-// Route files
-const bootcamps = require('./routes/bootcamp')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDb = require('./config/db')
 
 // LOAD environment variables
 dotenv.config({
-    path: './config/config.env'
+  path: './config/config.env',
 })
+
+//connect to database
+connectDb()
+// Route files
+const bootcamps = require('./routes/bootcamp')
 
 const app = express()
 
 // Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
 
 //Mount router
@@ -21,4 +26,8 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 9000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+)
+
